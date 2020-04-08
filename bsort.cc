c6326201a7875c406e9239572bc08a0bfcf3235c
Fede112 <fedebarone@gmail.com>
2020-04-08 15:35:57 +0200

    Changed variable type to support bigger files (uint64_t rows)

--- bsort.cc
+++ bsort.cc
@@ -1,21 +1,22 @@
 #include <stdlib.h>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <cstring> // memcpy
 #include <unistd.h>
 
 
-
-unsigned char * RadixSort(unsigned char * pData, std::size_t count, std::size_t record_size, std::size_t key_size, std::size_t key_offset = 0)
+unsigned char * RadixSort(unsigned char * pData, uint64_t count, uint32_t record_size, 
+                            uint32_t key_size, uint32_t key_offset = 0)
 {
     typedef uint8_t k_t [key_size];
     typedef uint8_t record_t [record_size];
 
 
-    size_t mIndex[key_size][256] = {0};            /* index matrix */
+    uint64_t mIndex[key_size][256] = {0};            /* index matrix */
     unsigned char * pTemp = new unsigned char [count*sizeof(record_t)];
     unsigned char * pDst, * pSrc;
-    size_t i,j,m,n;
+    uint64_t i,j,m,n;
     k_t u;
     record_t v;
 
@@ -63,30 +64,33 @@ unsigned char * RadixSort(unsigned char * pData, std::size_t count, std::size_t 
 int main(int argc, char *argv[])
 {
     int opt;
-    unsigned int record_size{4};
-    unsigned int key_size{4};
-    unsigned int key_offset{0};
+    uint32_t record_size{4};
+    uint32_t key_size{4};
+    uint32_t key_offset{0};
     std::string output{"outfile.bin"}; 
 
-    while ((opt = getopt(argc, argv, "r:k:s:o:")) != -1) 
+    while ((opt = getopt(argc, argv, "hr:k:s:o:")) != -1) 
     {
         switch (opt) 
         {
-        // case 'v':
-          // verbosity += 1;
-          // break;
         case 'r':
-            record_size = atoi(optarg);
+            record_size = strtoul(optarg, NULL, 10);
+            std::cout << record_size << std::endl;
             break;
         case 'k':
-            key_size = atoi(optarg);
+            key_size = strtoul(optarg, NULL, 10);
+            std::cout << key_size << std::endl;
             break;
         case 's':
-            key_offset = atoi(optarg);
+            key_offset = strtoul(optarg, NULL, 10);
+            std::cout << key_offset << std::endl;
             break;
         case 'o':
             output = optarg;
             break;
+        case 'h':
+            // go to default
+
         default: /* '?' */
             fprintf(stderr, "Usage: %s [-r record length in bytes] [-k key length in bytes] [-s key initial byte] [-o output filename] filename \n",
                     argv[0]);
@@ -103,8 +107,8 @@ int main(int argc, char *argv[])
 
 
     std::ifstream infile (argv[optind], std::ifstream::binary);
-    unsigned long int inbytes{0};
-    unsigned long int lines;
+    uint64_t inbytes{0};
+    uint64_t lines;
     char * bufferA;
     if (infile)
     {
